e97b74bb22d31a90cf7a763ca0dbcbf276403868
Robinson Roa <roa.robinson97@gmail.com>
2021-08-27 11:47:07 -0400

    first commit

--- graph/resolver.go
+++
@@ -1,9 +0,0 @@
-package graph
-
-import "github.com/roarobinson97/graphql-server/graph/model"
-
-//go:generate go run github.com/99designs/gqlgen
-
-type Resolver struct {
-	videos []*model.Video
-}
